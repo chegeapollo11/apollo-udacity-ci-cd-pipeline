5137711decab5d941d598bcf79995dc7d76d0d6d
Apollo Chege <chegeapollo@gmail.com>
2021-06-11 22:19:21 +0300

    Use a relative path in locust and path the host at runtime

--- locustfile.py
+++ locustfile.py
@@ -14,4 +14,4 @@ class MLUser(HttpUser):
     @task
     def test_predict(self):
         payload = {"CHAS":{"0":0},"RM":{"0":6.575},"TAX":{"0":296},"PTRATIO":{"0":15.3},"B":{"0":396.9},"LSTAT":{"0":4.98}}
-        self.client.post("http://localhost:5000/predict", json = payload)
+        self.client.post("/predict", json = payload)
